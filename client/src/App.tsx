4348106597dd7fd5535058b068d6692c72dfedd4
Victor Amano Izawa <victorsjc@gmail.com>
2025-04-08 23:05:15 -0300

    atualizacao de politica de privacidade

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,6 +17,7 @@ import CheckinAvulsoPage from "@/pages/checkin-avulso-page";
 import CheckinConfirmationPage from "@/pages/checkin-confirmation-page";
 import QrScannerPage from "@/pages/qr-scanner-page";
 import VacationAddPage from "@/pages/vacation-add-page";
+import PrivacyPolicy from "@/pages/privacy-policy";
 
 // Admin pages
 import AdminPage from "@/pages/admin-page";
@@ -27,7 +28,6 @@ import AdminTripsAdd from "@/pages/admin-trips-add";
 import AdminTripDetails from "@/pages/admin-trip-details";
 import AdminLocations from "@/pages/admin-locations";
 import AdminTimes from "@/pages/admin-times";
-import PrivacyPolicy from "@/pages/privacy-policy";
 
 // Route that redirects to dashboard if authenticated, otherwise shows landing page
 function HomeRoute() {
