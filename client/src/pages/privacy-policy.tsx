68698ed32e45db8fa5187c9d0ae10671710122c3
Victor Amano Izawa <victorsjc@gmail.com>
2025-04-08 23:58:46 -0300

    atualizacao de politica de privacidade

--- client/src/pages/privacy-policy.tsx
+++ client/src/pages/privacy-policy.tsx
@@ -9,30 +9,23 @@ export default function PrivacyPolicy() {
     <div className="min-h-screen flex flex-col">
 
       <section className="py-16 px-6 bg-gray-50">
-        <p>A sua privacidade é importante para nós. É política do TripCheck respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site 
+        <p>A sua privacidade é importante para nós. É política do <b>TripCheck</b> respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site 
           <a href="https://ui-transportecheckin-app.vercel.app/" target="_blank">TripCheck</a>, e outros sites que possuímos e operamos.</p>
         <p>Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço. Fazemos isso por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que estamos coletando e como será usado.</p>
         <p>Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando armazenamos dados, protegemos dentro de meios comercialmente aceitáveis ​​para evitar perdas e roubos, bem como acesso, divulgação, cópia, uso ou modificação não autorizados.</p>
         <p>Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.</p>
-        <p>O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas 
-          <a href="https://politicaprivacidade.com/" target="_blank">políticas de privacidade</a>.
+        <p>O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas políticas de privacidade.
         </p>
         <p>Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos fornecer alguns dos serviços desejados.</p>
         <p>O uso continuado de nosso site será considerado como aceitação de nossas práticas em torno de privacidade e informações pessoais. Se você tiver alguma dúvida sobre como lidamos com dados do usuário e informações pessoais, entre em contato conosco.</p>      
-      </section>
-
-      <section className="py-16 px-6 bg-gray-50">
-        <h2>Compromisso do Usuário</h2>
+        <h2><b>Compromisso do Usuário</b></h2>
         <p>O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o TripCheck oferece no site e com caráter enunciativo, mas não limitativo:</p>
         <ul>
           <li>A) Não se envolver em atividades que sejam ilegais ou contrárias à boa fé e à ordem pública;</li>
           <li>B) Não difundir propaganda ou conteúdo de natureza racista, xenofóbica ou qualquer tipo de pornografia ilegal, apologia ao terrorismo ou contra os direitos humanos;</li>
           <li>C) Não causar danos aos sistemas físicos (hardwares) e lógicos (softwares) do TripCheck, de seus fornecedores ou terceiros, para introduzir ou disseminar vírus informáticos ou quaisquer outros sistemas que possam causar danos.</li>
         </ul>
-      </section>
-
-      <section className="py-16 px-6 bg-gray-50">
-        <h2>Mais Informações</h2>
+        <h2><b>Mais Informações</b></h2>
         <p>Esperamos que estas informações estejam claras. Se houver algo que você não tem certeza se precisa ou não, geralmente é mais seguro deixar os cookies ativados, caso interaja com um dos recursos que você usa em nosso site.</p>
         <p>Esta política é efetiva a partir de 9 de abril de 2025.</p>
       </section>      
