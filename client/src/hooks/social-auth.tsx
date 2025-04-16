be6d050864edf0dd8e960c4d5a77bdbd80bffd5f
Victor Amano Izawa <victorsjc@gmail.com>
2025-04-15 23:33:35 -0300

    atualizacao social login

--- client/src/hooks/social-auth.tsx
+++ client/src/hooks/social-auth.tsx
@@ -1,12 +1,45 @@
 import React from "react";
 import { GoogleOAuthProvider, useGoogleLogin } from "@react-oauth/google";
+import { getQueryFn, apiRequest, queryClient } from "../lib/queryClient";
 
 export function SocialLogin ({ children }: { children: ReactNode }) {
   const handleLogin = useGoogleLogin({
     flow: "auth-code", // Define que queremos o código de autorização
-    onSuccess: (response) => {
-      console.log("Authorization Code Response:", response);
+    scope: "openid email profile",
+    access_type: "offline",
+    onSuccess: async (response) => {
+      console.log("Authorization Code Response:", response.code);
       // Aqui você pode enviar o response.code para o servidor
+      try {
+
+            // Corpo da requisição
+          const bodyData = new URLSearchParams();
+          bodyData.append("client_id", "627127621175-td1fqlg7dfkm4bm3ljbi8q9svuoe3f4b.apps.googleusercontent.com");
+          bodyData.append("client_secret", "INPTQn3uLwJxYQ2CRbhhS30w");
+          bodyData.append("code", response.code);
+          bodyData.append("redirect_uri", "https://ui-transportecheckin-app.vercel.app/");
+          bodyData.append("grant_type", "authorization_code");
+
+          try {
+            // Realizando a requisição POST
+            const response = await fetch("https://oauth2.googleapis.com/token", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+              },
+              body: bodyData.toString(),
+            });
+
+            if (response.ok) {
+              const data = await response.json();
+              setTokens(data);
+              console.log("Tokens Recebidos:", data);
+            } else {
+              console.error("Erro na troca de código:", response.status, await response.text());
+            }
+          } catch (error) {
+            console.error("Erro ao realizar a requisição:", error);
+          }
     },
     onError: (error) => {
       console.error("Login Failed:", error);
